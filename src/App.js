1952b06bdab7f86f142a3c0ab03204b99cc5b3df
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-24 13:20:22 -0400

    first page done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Nav from "./components/Nav";
 import Landing from './components/Landing.jsx'
 import Highlights from './components/Highlights.jsx'
 import Featured from './components/Featured.jsx'
+import Discounted from "./components/Discounted";
+import Explore from "./components/Explore.jsx"
+import Footer from "./components/Footer.jsx"
 
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Landing />
       <Highlights />
       <Featured />
+      <Discounted />
+      <Explore />
+      <Footer />
     </div>
   );
 }
--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -15,21 +15,23 @@ const Book = ({ book }) => {
         </a>
       </div>
       <div className="book__ratings">
-        <FontAwesomeIcon icon="star" />
-        <FontAwesomeIcon icon="star" />
-        <FontAwesomeIcon icon="star" />
-        <FontAwesomeIcon icon="star" />
-        <FontAwesomeIcon icon="star-half-alt" />
+        {new Array(Math.floor(book.rating)).fill(0).map((_, index) => (
+          <FontAwesomeIcon icon="star" key={index} />
+        ))}
+        {!Number.isInteger(book.rating) && (
+          <FontAwesomeIcon icon="star-half-alt" />
+        )}
       </div>
       <div className="book__price">
         {book.salePrice ? (
           <>
-            {" "}
-            <span className="book__price--normal">${book.OriginalPrice}</span>$
-            {book.salePrice}
+            <span className="book__price--normal">
+              ${book.originalPrice.toFixed(2)}
+            </span>
+            ${book.salePrice.toFixed(2)}
           </>
         ) : (
-          "null"
+          <> ${book.originalPrice.toFixed(2)}</>
         )}
       </div>
     </div>
