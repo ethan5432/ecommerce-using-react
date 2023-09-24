bb8284847f36b1b4d3b7438a0178841746c31e1e
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-24 17:30:19 -0400

    setting up second page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import Nav from "./components/Nav";
-import Landing from './components/Landing.jsx'
-import Highlights from './components/Highlights.jsx'
-import Featured from './components/Featured.jsx'
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore.jsx"
-import Footer from "./components/Footer.jsx"
-
+import Footer from "./components/Footer.jsx";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div>
-      <Nav></Nav>
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div>
+        <Nav />
+        <Route path='/' exact component={Home} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
