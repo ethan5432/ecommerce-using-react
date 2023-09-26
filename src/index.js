42309db9395de3c68eede866df7101e852120baa
ethan5432 <ethan.nirenberg@gmail.com>
2023-09-26 13:42:58 -0400

    finished book page. onto reccomnded books

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags, faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons'
+import {
+  faBars, faShoppingCart, faTimes, faBolt,
+  faBookOpen, faTags, faStar, faStarHalfAlt, faArrowLeft
+} from '@fortawesome/free-solid-svg-icons'
 
 library.add(
   faBars,
@@ -14,7 +17,7 @@ library.add(
   faBookOpen,
   faTags,
   faStar,
-  faStarHalfAlt
+  faStarHalfAlt, faArrowLeft
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
